bccaa370a79801f1969e75facb16ab1f7d1be901
안은결 <eungyeole@naver.com>
2022-06-06 03:45:04 +0900

    fix: test

--- services/apply/next.config.js
+++ services/apply/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    basePath: '/apply',
     experimental: {
         externalDir: true,
         outputStandalone: true,
