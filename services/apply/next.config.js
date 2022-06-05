5b602c589b2e08fa9201792432e001df4ecaeff1
안은결 <eungyeole@naver.com>
2022-06-06 04:05:00 +0900

    chore: push-service github workflow 작성 (#19)
    fix: nmae -> name 오타 수정
    
    
    fix: steps deths 조정
    
    
    feat(apply): Docker file 임시 추가
    
    fix: push-service 오타 수정
    
    
    fix: env 임시 추가
    
    
    fix: 옵션도 임시 추가
    
    
    fix: push-service getVersion 오류 수정
    
    
    fix: test
    
    
    fix: test

--- services/apply/next.config.js
+++ services/apply/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
     reactStrictMode: true,
+    basePath: process.env.NODE_ENV === 'production' ? '/apply' : undefined,
     experimental: {
         externalDir: true,
+        outputStandalone: true,
     },
 };
