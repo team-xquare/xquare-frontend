11d02bd9a9cb3c97ebf461970ebbe40d47d653d2
안은결 <eungyeole@naver.com>
2022-07-02 04:40:33 +0900

    feat: xbridge confirm 추가 및 타입 추론 보강

--- services/xbridge-test/src/pages/index.tsx
+++ services/xbridge-test/src/pages/index.tsx
@@ -9,6 +9,23 @@ const Home: NextPage = () => {
         <div>
             <XbridgeImage src="https://cdnimg.melon.co.kr/cm2/artistcrop/images/002/61/143/261143_20210325180240_500.jpg?61e575e8653e5920470a38d1482d7312/melon/resize/416/quality/80/optimize" />
             <XbridgeImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKS1BGRjgU289BZuhSFI4V7GBh6Ny_UzgH6A&usqp=CAU" />
+
+            <Button
+                onClick={() => {
+                    const data = sendBridgeEvent(
+                        'confirm',
+                        {
+                            message: '브릿지를 테스트 하시겠습니까?',
+                            confirmText: 'ㅇㅇ',
+                            cacelText: 'ㄴㄴ',
+                        },
+                        ({ data }) => confirm(data.message),
+                    );
+
+                    console.log(data);
+                }}>
+                모달
+            </Button>
             <Button
                 onClick={() =>
                     sendBridgeEvent('navigate', '/back', ({ data }) => router.push(data))
--- shared/xbridge/src/index.ts
+++ shared/xbridge/src/index.ts
@@ -1,18 +1,35 @@
 import { Device } from '@xquare/utils';
+import { useEffect } from 'react';
 
-type BridgeType = 'navigate' | 'back' | 'imageDetail';
+type BridgeType = 'navigate' | 'back' | 'imageDetail' | 'confirm';
+
+interface XBridgeSendData extends Record<BridgeType, unknown> {
+    navigate: string;
+    imageDetail: string[];
+    back: boolean;
+    confirm: {
+        message: string;
+        confirmText: string;
+        cacelText: string;
+    };
+}
+
+interface XBridgeResponseData extends Record<BridgeType, unknown> {
+    confirm: {
+        success: boolean;
+    };
+}
 
 export interface BrowserActionParameters<T> {
     bridge: BridgeType;
     data: T;
 }
-export const sendBridgeEvent = <T extends unknown>(
-    bridge: BridgeType,
-    data: T,
-    browserAction?: (params: BrowserActionParameters<T>) => any,
+export const sendBridgeEvent = <T extends BridgeType>(
+    bridge: T,
+    data: XBridgeSendData[T],
+    browserAction?: (params: BrowserActionParameters<XBridgeSendData[T]>) => any,
 ) => {
     const globalThis = window as any;
-    console.log(data);
     if (Device.isMobileWeb()) {
         if (Device.getOSByUserAgent() === 'ios')
             globalThis.webkit.messageHandlers[bridge].postMessage(data);
@@ -20,6 +37,21 @@ export const sendBridgeEvent = <T extends unknown>(
             globalThis.webview[bridge](data);
         }
     } else {
-        browserAction && browserAction({ bridge, data });
+        return browserAction?.({ bridge, data });
     }
+};
+
+export const useBridgeHandler = <T extends BridgeType>(
+    bridge: T,
+    callback: (event: CustomEvent<XBridgeResponseData[T]>) => any,
+) => {
+    useEffect(() => {
+        const onCallback = ((event: CustomEvent<XBridgeResponseData[T]>) => {
+            callback(event);
+        }) as EventListener;
+
+        window.addEventListener(`${bridge}XBridge`, onCallback);
+
+        return () => window.removeEventListener(`${bridge}XBridge`, onCallback);
+    }, [callback, bridge]);
 };
