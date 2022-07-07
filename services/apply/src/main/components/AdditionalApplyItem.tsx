e1908659b9ec032240f48b6d6d8fe34729197ed4
안은결 <eungyeole@naver.com>
2022-07-07 16:11:24 +0900

    fix(apply): 자습실 신청 bridge 연결

--- services/apply/src/main/components/AdditionalApplyItem.tsx
+++ services/apply/src/main/components/AdditionalApplyItem.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import styled from '@emotion/styled';
 import { FlexCol, FlexRow } from '../../common/Flexbox';
 import { Botton, Caption } from '@semicolondsm/ui';
+import { sendBridgeEvent } from '@shared/xbridge';
+import { useRouter } from 'next/router';
 
 interface AdditionalApplyItemProps {
     daliy: string;
@@ -10,32 +12,38 @@ interface AdditionalApplyItemProps {
 }
 
 const AdditionalApplyItem = ({ daliy, applyKind, linkTo }: AdditionalApplyItemProps) => {
+    const router = useRouter();
     return (
-        <Link href={linkTo}>
-            <AdditionalApplyCardContainer>
-                <FlexCol gap={12}>
-                    <FlexCol>
-                        <Caption color="gray700">{daliy}</Caption>
-                        <Botton>{applyKind}</Botton>
-                    </FlexCol>
-                    <FlexRow justify="flex-end" fullWidth>
-                        <IconWrapper></IconWrapper>
-                    </FlexRow>
+        <AdditionalApplyCardContainer
+            onClick={(e) => {
+                e.preventDefault();
+                sendBridgeEvent('navigate', linkTo, ({ data }) => router.push(data));
+            }}
+            href={linkTo}>
+            <FlexCol gap={12}>
+                <FlexCol>
+                    <Caption color="gray700">{daliy}</Caption>
+                    <Botton>{applyKind}</Botton>
                 </FlexCol>
-            </AdditionalApplyCardContainer>
-        </Link>
+                <FlexRow justify="flex-end" fullWidth>
+                    <IconWrapper></IconWrapper>
+                </FlexRow>
+            </FlexCol>
+        </AdditionalApplyCardContainer>
     );
 };
 
 export default AdditionalApplyItem;
 const AdditionalApplyCardContainer = styled.a`
     background-color: ${({ theme }) => theme.colors.gray50};
+    text-decoration: none;
     padding: 16px;
     width: 136px;
     height: 126px;
     border-radius: 12px;
+    transition: 0.3s;
     cursor: pointer;
-    &:hover {
+    &:active {
         background-color: ${({ theme }) => theme.colors.gray100};
     }
 `;
