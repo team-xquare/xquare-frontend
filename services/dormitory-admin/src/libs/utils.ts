611b7f567ca92325ed12f401109965cbc3bdaa57
Sung Ye In <yisung@superb-ai.com>
2022-09-06 23:11:24 +0900

    feat(dormitory-admin): 학생 검색 및 액셀 다운로드 선택 추가

--- services/dormitory-admin/src/libs/utils.ts
+++ services/dormitory-admin/src/libs/utils.ts
@@ -32,3 +32,48 @@ export const getDateString = () => {
     const now = new Date();
     return `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDay()}`;
 }
+
+const reRegExpChar = /[\\^$.*+?()[\]{}|]/g;
+const reHasRegExpChar = RegExp(reRegExpChar.source);
+
+export const escapeRegExp = (string: string) => {
+  return (string && reHasRegExpChar.test(string))
+  ? string.replace(reRegExpChar, '\\$&')
+  : (string || '')
+}
+
+export const ch2pattern = (ch: string) => {
+  const offset = 44032;
+  if (/[가-힣]/.test(ch)) {
+    const chCode = ch.charCodeAt(0) - offset;
+    if (chCode % 28 > 0) {
+      return ch;
+    }
+    const begin = Math.floor(chCode / 28) * 28 + offset;
+    const end = begin + 27;
+    return `[\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+  }
+  if (/[ㄱ-ㅎ]/.test(ch)) {
+    const con2syl = {
+      'ㄱ': '가'.charCodeAt(0),
+      'ㄲ': '까'.charCodeAt(0),
+      'ㄴ': '나'.charCodeAt(0),
+      'ㄷ': '다'.charCodeAt(0),
+      'ㄸ': '따'.charCodeAt(0),
+      'ㄹ': '라'.charCodeAt(0),
+      'ㅁ': '마'.charCodeAt(0),
+      'ㅂ': '바'.charCodeAt(0),
+      'ㅃ': '빠'.charCodeAt(0),
+      'ㅅ': '사'.charCodeAt(0),
+    } as Record<string, number>;
+    const begin = con2syl[ch] || ( ( ch.charCodeAt(0) - 12613) * 588 + con2syl['ㅅ'] );
+    const end = begin + 587;
+    return `[${ch}\\u${begin.toString(16)}-\\u${end.toString(16)}]`;
+  }
+  return escapeRegExp(ch); //5. 함수 개조
+}
+
+export const createFuzzyPattern = (query: string) => {
+    const pattern = query.split('').map(ch2pattern).join('.*?');
+    return new RegExp(pattern);
+}
