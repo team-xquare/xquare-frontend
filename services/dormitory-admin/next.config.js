df483636cdb41b819efcb31f18c610554aa46f07
Sung Ye In <yisung@superb-ai.com>
2022-09-14 22:04:50 +0900

    chore(dormitory-admin): 배포를 위한 수정

--- services/dormitory-admin/next.config.js
+++ services/dormitory-admin/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    experimental: {
-        outputStandalone: true,
-    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
