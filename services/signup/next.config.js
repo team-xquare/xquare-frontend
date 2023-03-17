ce0502e32608b173765b3360a4e77202708e337f
kimuichan <leerabi577@gmail.com>
2023-03-17 09:04:33 +0900

    fix(signup): next config root로 수정 [skip ci]

--- services/signup/next.config.js
+++ services/signup/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    basePath: process.env.NODE_ENV === 'production' ? '/signup' : undefined,
     experimental: {
         externalDir: true,
     },
