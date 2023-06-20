d057f4557a6e6737403fc9132d6df6b19cf643ab
kimjh11130 <kimjh11130@naver.com>
2023-06-20 11:02:24 +0900

    feat(apply): 에러메시지 추가

--- services/apply/src/class-move/hooks/useClassroomMove.ts
+++ services/apply/src/class-move/hooks/useClassroomMove.ts
@@ -2,6 +2,7 @@ import { useMutation } from 'react-query';
 import useTimeTable from '../../common/hooks/useTimeTable';
 import { postClassroomMove } from '../apis';
 import { sendBridgeEvent } from '@shared/xbridge';
+
 const useClassroomMove = () => {
     const currentPeriod = useTimeTable();
     return useMutation(
@@ -10,9 +11,36 @@ const useClassroomMove = () => {
             onSuccess: () => {
                 sendBridgeEvent('back', true);
             },
-            onError: () => {
+            onError: (e: any) => {
+                let message = '교실이동 신청을 실패하였습니다.';
+                const errorMessage = e.response?.data?.message;
+                switch (e.response?.status) {
+                    case 400:
+                        switch (errorMessage) {
+                            case 'Cannot Movement':
+                                message = '교실이동은 한번만 신청할수 있습니다.';
+                                break;
+                            case 'After School Cannot Movement':
+                                message = '방과후에는 교실이동을 할 수 없습니다.';
+                                break;
+                        }
+                        break;
+                    case 401:
+                        message = '주말에는 교실이동을 할 수 없습니다.';
+                        break;
+                    case 409:
+                        switch (errorMessage) {
+                            case 'Class not found':
+                                message = '존재하지 않는 교실입니다.';
+                                break;
+                            case 'Time not found':
+                                message = '학교 수업시간에만 이동 가능합니다.';
+                                break;
+                        }
+                        break;
+                }
                 sendBridgeEvent('error', {
-                    message: '교실이동 신청을 실패하였습니다.',
+                    message: message,
                 });
             },
         },
--- services/apply/src/today-out/hooks/useTodayOutApply.ts
+++ services/apply/src/today-out/hooks/useTodayOutApply.ts
@@ -1,14 +1,26 @@
 import { useMutation } from 'react-query';
 import todayOutApplyRequest from '../apis';
 import { sendBridgeEvent } from '@shared/xbridge';
+import axios from 'axios';
 const useTodayOutApply = () => {
     return useMutation(todayOutApplyRequest, {
         onSuccess: () => {
             sendBridgeEvent('back', true);
         },
-        onError: () => {
+        onError: (e) => {
+            let errorMessage = '외출 신청을 실패하였습니다.';
+            if (axios.isAxiosError(e)) {
+                switch (e.response?.status) {
+                    case 401:
+                        errorMessage = '주말에는 주말 외출 신청을 이용해주세요.';
+                        break;
+                    case 409:
+                        errorMessage = '외출 신청은 한번만 신청할수 있습니다.';
+                        break;
+                }
+            }
             sendBridgeEvent('error', {
-                message: '외출 신청을 실패하였습니다.',
+                message: errorMessage,
             });
         },
     });
