ae9dd59939d2405c7c76362532405ee28edb1c26
inung1004 <dksdbswl10430@naver.com>
2023-08-23 09:21:19 +0900

    fix(xdesign-story): Switch storybook 재작성

--- services/xdesign-story/src/components/Switch/Switch.stories.tsx
+++ services/xdesign-story/src/components/Switch/Switch.stories.tsx
@@ -1,27 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
 import { Switch } from '.';
-import { useRef, useState } from 'react';
 
-export default {
-    title: 'components/Switch',
+const meta: Meta<typeof Switch> = {
+    title: 'Components/Switch',
     component: Switch,
 };
 
-export const Default = () => {
-    const [isOn1, setIsOn1] = useState<boolean>(false);
-    const ref = useRef<HTMLButtonElement | null>(null);
+export default meta;
+type Story = StoryObj<typeof Switch>;
 
-    return (
-        <>
-            <h2>Switch</h2>
-            <br />
-
-            <h3>{`${isOn1}`}</h3>
-            <Switch isOn={isOn1} onClick={() => setIsOn1(!isOn1)} disabled={false} ref={ref} />
-            <br />
-            <h3>비활성화되었을 시 40%의 alpha 값을 가집니다.</h3>
-            <Switch isOn={true} onClick={() => {}} disabled={true} />
-            <br />
-            <Switch isOn={false} onClick={() => {}} disabled={true} />
-        </>
-    );
+export const Default: Story = {
+    args: { disabled: false, isOn: false },
 };
