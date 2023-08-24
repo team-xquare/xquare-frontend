48a070deb0dbfa65f71f28cbc712a348516ddd82
kimjh11130 <kimjh11130@naver.com>
2023-08-25 01:27:15 +0900

    build test..

--- services/feed/src/utils/axios.ts
+++ services/feed/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const testToken = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNjMyNjEzMi0zMDM1LTM0MzctMmQzMy0zMDY1NjQyZDM0MzUiLCJyb2xlIjoiU1RVIiwiZXhwIjoxNjkyOTAxMjY5LCJ0eXBlIjoiQUNDRVNTX1RPS0VOIiwiYXV0aG9yaXRpZXMiOlsiVUtOIiwiU1RVIl19.0h-5ObWTf5QoDMaeudEGcgJUNo6fzCWmkDvV2zZg5ws`;
+const testToken = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNjMyNjEzMi0zMDM1LTM0MzctMmQzMy0zMDY1NjQyZDM0MzUiLCJyb2xlIjoiU1RVIiwiZXhwIjoxNjkyOTAxNTg2LCJ0eXBlIjoiQUNDRVNTX1RPS0VOIiwiYXV0aG9yaXRpZXMiOlsiVUtOIiwiU1RVIl19.QOB2Vp1ZG0S9XGHm7BvHzsAwAkn0k6fJBdEY7Yjl9rQ`;
 export const instance = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/feeds`,
     timeout: 10000,
